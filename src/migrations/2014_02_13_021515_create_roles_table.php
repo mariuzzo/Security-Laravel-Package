0006117cbfdb4dedb1ab921a201fa375098f9adc
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-02-12 22:20:44 -0400

    Roles migration script created.

---
+++ src/migrations/2014_02_13_021515_create_roles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends AbstractMigration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('roles', function($table)
+		{
+			$table->increments('id');
+			$table->string('name', 100)->unique();
+			$table->string('description', 200);
+		});
+
+		$this->addCommonsTo('roles');
+
+		Schema::table('users', function($table)
+		{
+			$table->foreign('role_id')->references('id')->on('roles');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->dropForeign('users_role_id_foreign');
+		});
+
+		Schema::drop('roles');
+	}
+
+}
