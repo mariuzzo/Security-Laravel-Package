20aef28639cdd6a4b6e9e38eedd8a85ba34d5c87
German Popoter <g.popoteur@gmail.com>
2014-03-04 12:39:55 +0100

    Added validation to the data passed via the promp

--- src/Mariuzzo/Security/Command/UserCreateCommand.php
+++ src/Mariuzzo/Security/Command/UserCreateCommand.php
@@ -3,6 +3,7 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Illuminate\Support\Facades\Validator;
 
 class UserCreateCommand extends Command {
 
@@ -45,14 +46,50 @@ class UserCreateCommand extends Command {
 		$first_name            = $this->ask('Type the first name:');
 		$last_name             = $this->ask('Type the last name:');
 		$status                = $this->ask('Type the status:');
+		$role_id			   = $this->ask('Type the Role Id:');
+
+		// Validation Rules
+		$rules = array(
+			'username' 				=> 'required',
+			'password' 				=> 'required|min:4',
+			'password_confirmation' => 'required|same:password',
+			'email' 				=> 'required|email|unique:users',
+			'first_name' 			=> 'required',
+			'last_name' 			=> 'required',
+			'status' 				=> 'required',
+			'role_id' 				=> 'required'
+		);
+
+		// Data
+		$data = compact('username', 'password', 'password_confirmation', 
+			'email', 'first_name', 'last_name', 'status');
+
+		// Validator
+		$validator = Validator::make($data, $rules);
+
+		// If validation fails
+		// show errors
+		if($validator->fails()){
+			$messages = $validator->messages();
+
+			foreach ($messages->all() as $message)
+			{
+			    $this->error($message);
+			}
+
+			return;
+		}
 
 		// Hash the password.
 		$password = \Hash::make($password);
 
 		// Insert new user to database.
 		\DB::table('users')->insert(
-			compact('username', 'password', 'email', 'first_name', 'last_name', 'status')
+			compact('username', 'password', 'email', 'first_name', 
+				'last_name', 'status', 'role_id')
 		);
+
+		$this->info('User created!');
 	}
 
 	/**
